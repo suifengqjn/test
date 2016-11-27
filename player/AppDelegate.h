30f7f10273812a435d4d1b6b5fafb42062a99328
qianjianeng <329426491@qq.com>
2016-11-27 22:02:56 +0800

    update

---
+++ player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  player
+//
+//  Created by qianjn on 2016/11/27.
+//  Copyright © 2016年 SF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
